ce2fc7360102f065e4c87995311faa575c9fee8d
Jack Quinoa <jquinoa208@gmail.com>
2020-05-27 23:23:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,9 +29,9 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre] then 
-       	genre_count = genre_count[genre]
+       	genre_count[genre] = 1 
       else 
-        genre_count
+        genre_count[genre] << +1 
       end
     end
     genre_count
