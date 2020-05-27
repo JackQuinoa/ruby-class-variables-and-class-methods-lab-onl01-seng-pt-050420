37b2249a9fc92eba9fd31c9411bcdfb05e484ca7
Jack Quinoa <jquinoa208@gmail.com>
2020-05-27 23:45:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,6 +38,11 @@ class Song
   end
   
   def self.artist_count 
-    
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist] then
+        artist_count[artist] += 1 
+      else
+        artis_count[artist] = 1 
   end
 end
